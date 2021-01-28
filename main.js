e501b0ac00b8019bb69ea88a89028b78b7c3e1b0
LauraQuantrille <lquantrille@gmail.com>
2021-01-27 20:26:39 -0500

    math functions added & working

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-const button = document.querySelectorAll('[data-number]')
+// const button = document.querySelectorAll('[data-number]')
 
 let display = document.querySelector(".display")
-display.innerHTML = "test"
+display.innerHTML = ""
 
 
 
 let buttonsArray = document.querySelectorAll("button")
 for (let key of buttonsArray) {
     key.addEventListener('click', event => {
-        console.log(event.target);
-        display.innerHTML = key;
+        if (event.target.innerHTML === '=') {
+            console.log(display.innerHTML)
+            display.innerHTML = eval(display.innerHTML);
+        }
+        else if (event.target.innerHTML === 'C') {
+            display.innerHTML = ' '
+        }
+        else if (display.innerHTML === '0' && event.target.innerHTML === '0') {
+            display.innerHTML = '0'
+        }
+        else if (event.target.innerHTML === 'X') {
+            display.innerHTML = [display.innerHTML, '*'].join('');
+        }
+        else {
+            display.innerHTML = [display.innerHTML, event.target.innerHTML].join('');
+        }
+
+
+
+
+
+
+        // display.innerHTML = [display.innerHTML, event.target.innerHTML].join('')
+
+        // console.log(event.target.innerText);
+
     });
-
-};
-
-
+}
